2fc614461da479a051479e2e72a241b8dac2879b
Yoon-Ki Hong <mymy49@nate.com>
2025-02-12 19:28:14 +0900

    스케줄러만 동작하는 기본 코드 작성

---
+++ app/src/dev/dev_led.cpp
@@ -0,0 +1,25 @@
+#include <dev/led.h>
+#include <yss/instance.h>
+
+namespace led
+{
+	void initialize(void)
+	{
+		setLedR(false);
+		setLedY(false);
+		setLedG(false);
+	}
+
+	void setLedR(bool on)
+	{
+	}
+
+	void setLedY(bool on)
+	{
+	}
+
+	void setLedG(bool on)
+	{
+	}
+}
+
---
+++ app/src/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025 Yoon-Ki Hong
+ *
+ * This file is subject to the terms and conditions of the MIT License.
+ * See the file "LICENSE" in the main directory of this archive for more details.
+ */
+
+#include <yss.h>
+#include <bsp.h>
+
+int main(void)
+{
+	// 운영체체 초기화
+	initializeYss();
+
+	// 보드 초기화
+	initializeBoard();
+
+	while(1)
+	{
+		thread::yield();
+	}
+}
+
