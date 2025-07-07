c3743869e141ac97c2e66d913c77b0cb4869cacb
Yoon-Ki Hong <mymy49@nate.com>
2025-07-07 22:16:08 +0900

    UART 테스트 코드 추가
    - UART0에 Hello World!! 출력 코드 추가

--- app/src/bsp.cpp
+++ app/src/bsp.cpp
@@ -10,6 +10,21 @@
 
 void initializeBoard(void)
 {
-		
+	// UART0 초기화
+	Uart::config_t uart0Config =
+	{
+		Uart::MODE_NORMAL,	//mode_t mode;			// 동작 모드의 종류를 설정합니다.
+		115200,				//uint32_t baudrate;		// 보레이트를 설정합니다.
+		Uart::STOP_1BIT,	//stopbit_t stopbit;		// Stop Bit의 종류를 설정합니다.
+		nullptr,			//void *rcvBuf;			// 수신 버퍼를 지정합니다.
+		128				//uint32_t rcvBufSize;	// 수신 버퍼의 크기를 지정합니다.
+	};
+
+	gpioB.setAsAltFunc(12, Gpio::PB12_UART0_RXD);
+	gpioB.setAsAltFunc(13, Gpio::PB13_UART0_TXD);
+
+	uart0.enableClock();
+	uart0.initialize(uart0Config);
+	uart0.enableInterrupt();	
 }
 
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -11,34 +11,36 @@
 #include <util/runtime.h>
 #include <std_ext/string.h>
 
+void thread_testUart(void);
+
+const char *gHelloWord = "Hello World!!\n\r";
+uint32_t gSize = strlen(gHelloWord);
+
 int main(void)
 {
-	uint8_t sa[32], da[32];
-
-	for(uint32_t i = 0; i < 32; i++)
-	{
-		sa[i] = i;
-		da[i] = 0;
-	}
-		// 운영체체 초기화
+	// 운영체체 초기화
 	initializeYss();
 
 	// 보드 초기화
 	initializeBoard();
 
-	// DMA memory to memory Test
-	memcpyd(da, sa, 32);
-
-	memsetd(da, 0xAA, 32);
-
-	memsethwd(da, 0xBBCC, 16);
-
-	memsetwd(da, 0x12345678, 8);
-
+	thread::add(thread_testUart, 512);
+	
 	while(1)
 	{
-		debug_printf("%d\r", (uint32_t)runtime::getMsec());
+		if(uart0.getRxCount() > 0)
+		{
+			debug_printf("%c\r\n", uart0.getRxByte());
+		}
 		thread::yield();
+	}
+}
+
+void thread_testUart(void)
+{
+	while(1)
+	{
+		uart0.send(gHelloWord, gSize);
 	}
 }
 
