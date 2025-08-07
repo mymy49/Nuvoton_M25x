32e141939e1f355fff7d7db77fd0762d3f671b0f
Yoon-Ki Hong <mymy49@nate.com>
2025-08-07 17:56:57 +0900

    BPWM 동작 코드 추가
    - 고정된 PWM이 출력되는 간단한 코드 작성

--- app/inc/config.h
+++ app/inc/config.h
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Yoon-Ki Hong
+ * Copyright (c) 2015 Yoon-Ki Hong
  *
  * This file is subject to the terms and conditions of the MIT License.
  * See the file "LICENSE" in the main directory of this archive for more details.
  */
 
-// Nuvoton M483 시리즈에서 유효한 설정이 담긴 참고용 파일입니다.
+// Nuvoton M251 시리즈에서 유효한 설정이 담긴 참고용 파일입니다.
 // 해당 파일을 사용자 프로젝트의 include 경로(app/inc)에 복사하고 config.h로 변경해서 사용하는 것을 추천합니다.
 
 #ifndef YSS_CONFIG__H_
@@ -14,17 +14,18 @@
 // ####################### 외부 크리스탈 클럭 주파수 설정 #######################
 // 아래 HSE_CLOCK_FREQ가 정의 되어 있으면 HSE 클럭을 소스로 PLL 설정이 이뤄집니다.
 // 정의되어 있지 않으면 HSI 클럭을 소스로 PLL 설정이 이뤄집니다.
-//#define HSE_CLOCK_FREQ 32000000
+// 크리스탈은 반드시 4 MHz의 배수로 사용해야 합니다.
+//#define HSE_CLOCK_FREQ 12000000
 
 // ####################### 스케줄러 설정 #######################
-// runtime 함수를 지원할 PWM 장치 설정 (RUNTIME_TIMER0, 1, 2, 3, 4)
+// runtime 함수를 지원할 TIMER 장치 설정 (RUNTIME_TIMER0, 1, 2, 3)
 #define YSS_TIMER			RUNTIME_TIMER0
 
 // 쓰레드당 할당 받는 Systick Clock의 수
 #define THREAD_GIVEN_CLOCK	20000
 
 // 최대 등록 가능한 쓰레드의 수
-#define MAX_THREAD			12
+#define MAX_THREAD			16
 
 // 쓰레드의 스택을 0xAA 패턴으로 채우기 (true, false)
 #define FILL_THREAD_STACK	false
@@ -46,23 +47,21 @@
 #define BPWM0_ENABLE		true
 #define BPWM1_ENABLE		true
 
-// EPWM 활성화
-#define EPWM0_ENABLE		true
-#define EPWM1_ENABLE		false
+// I2C 활성화
+#define I2C0_ENABLE			false
+#define I2C1_ENABLE			false
 
 // I2S 활성화
 #define I2S0_ENABLE			false
-#define I2S1_ENABLE			false
 
 // PWM 활성화
 #define PWM0_ENABLE			false
 #define PWM1_ENABLE			false
 #define PWM2_ENABLE			false
-#define PWM3_ENABLE			true
+#define PWM3_ENABLE			false
 
 // SPI 활성화
 #define SPI0_ENABLE			false
-#define SPI1_ENABLE			true
 
 // TIMER 활성화
 #define TIMER0_ENABLE		false
@@ -72,12 +71,8 @@
 
 // UART 활성화
 #define UART0_ENABLE		true
-#define UART1_ENABLE		true
-#define UART2_ENABLE		true
-#define UART3_ENABLE		true
-
-// USBD 활성화
-#define USBD_ENABLE			true
+#define UART1_ENABLE		false
+#define UART2_ENABLE		false
 
 #endif
 
--- app/src/bsp.cpp
+++ app/src/bsp.cpp
@@ -25,6 +25,32 @@ void initializeBoard(void)
 
 	uart0.enableClock();
 	uart0.initialize(uart0Config);
-	uart0.enableInterrupt();	
+	uart0.enableInterrupt();
+
+	// bpwm0 초기화
+	gpioA.setAsAltFunc(11, Gpio::PA11_BPWM0_CH0);	// PA11을 BPWM0 CH0으로 설정
+	gpioA.setAsAltFunc(10, Gpio::PA10_BPWM0_CH1);	// PA10을 BPWM0 CH1으로 설정
+	gpioA.setAsAltFunc(9, Gpio::PA9_BPWM0_CH2);		// PA9을 BPWM0 CH2으로 설정
+	gpioA.setAsAltFunc(8, Gpio::PA8_BPWM0_CH3);		// PA8을 BPWM0 CH3으로 설정
+	gpioA.setAsAltFunc(4, Gpio::PA4_BPWM0_CH4);		// PA8을 BPWM0 CH4으로 설정
+	gpioA.setAsAltFunc(5, Gpio::PA5_BPWM0_CH5);		// PA8을 BPWM0 CH5으로 설정
+
+	bpwm0.enableClock();							// BPWM0의 클럭 활성화
+	bpwm0.initialize(1000);							// BPWM0의 출력 주기를 1kHz로 초기화
+	bpwm0.setAsPwmOutput(0, true);					// BPWM0의 CH0을 PWM 반전 출력으로 설정
+	bpwm0.setAsPwmOutput(1);						// BPWM0의 CH1을 PWM 비반전 출력으로 설정
+	bpwm0.setAsPwmOutput(2);						// BPWM0의 CH2를 PWM 비반전 출력으로 설정
+	bpwm0.setAsPwmOutput(3);						// BPWM0의 CH3을 PWM 비반전 출력으로 설정
+	bpwm0.setAsPwmOutput(4);						// BPWM0의 CH4를 PWM 비반전 출력으로 설정
+	bpwm0.setAsPwmOutput(5);						// BPWM0의 CH5를 PWM 비반전 출력으로 설정
+	bpwm0.start();									// 타이머 카운터 시작
+
+	bpwm0.setDutyRatio(0, 0.1);						// BPWM0 CH0의 출력 듀티비를 10%로 설정
+	bpwm0.setDutyRatio(1, 0.2);						// BPWM0 CH1의 출력 듀티비를 20%로 설정
+	bpwm0.setDutyRatio(2, 0.3);						// BPWM0 CH2의 출력 듀티비를 30%로 설정
+	bpwm0.setDutyRatio(3, 0.4);						// BPWM0 CH3의 출력 듀티비를 40%로 설정
+	bpwm0.setDutyRatio(4, 0.5);						// BPWM0 CH4의 출력 듀티비를 50%로 설정
+	bpwm0.setDutyRatio(5, 0.6);						// BPWM0 CH5의 출력 듀티비를 60%로 설정
+
 }
 
